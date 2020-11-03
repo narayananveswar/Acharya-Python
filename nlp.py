24bdbe467325cc086e5da0567be94908de3768e2
narayananveswar <narayananveswar@gmail.com>
2020-11-04 02:14:45 +0530

    web creation

---
+++ nlp.py
@@ -0,0 +1,13 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+
+ex = 'European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices'
+
+def preprocess(sent):
+    sent = nltk.word_tokenize(sent)
+    sent = nltk.pos_tag(sent)
+    return sent
+
+sent = preprocess(ex)
+print(sent)
